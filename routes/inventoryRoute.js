2e937d7afe55cdd1bec214161bd1e1e67102a8f6
Marcela <lmvt.07@gmail.com>
2025-05-31 10:25:33 -0600

    Add intentional error route and controller for middleware testing

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -3,12 +3,15 @@ const express = require("express")
 const router = new express.Router() 
 const invController = require("../controllers/invController")
 const utilities = require("../utilities/index");
+const errorController = require("../controllers/errorController");
 
 // Route to build inventory by classification view
 router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId));
-
 //Route to build vehicle detail view
 router.get("/detail/:inventoryId", utilities.handleErrors(invController.buildByInventoryId));
 // router.get("/inventory/detail/:invId", invController.buildDetailView)
+// Intentional 500 error test route
+router.get("/trigger-error", utilities.handleErrors(errorController.throwError));
+
 
 module.exports = router;
