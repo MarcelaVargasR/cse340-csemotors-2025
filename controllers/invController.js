c941acd74343987bbc89c0bfb6b43b94185d4700
Marcela <lmvt.07@gmail.com>
2025-06-03 14:27:17 -0600

    implement add-classification view with full MVC and validation

--- controllers/invController.js
+++ controllers/invController.js
@@ -19,19 +19,65 @@ invCont.buildByClassificationId = async function (req, res, next) {
   });
 };
 
-
-
+/* ***************************
+ *  Build inventory by single view
+ * ************************** */
 invCont.buildByInventoryId = async function (req, res, next) {
-  const inv_id = req.params.inventoryId
-  const data = await invModel.getSingleByInventoryId(inv_id)
-  const view = await utilities.buildSingleView(data)
-  let nav = await utilities.getNav()
-  const vehicleName = data.inv_year + " " + data.inv_make + " " + data.inv_model
+  const inv_id = req.params.inventoryId;
+  const data = await invModel.getSingleByInventoryId(inv_id);
+  const view = await utilities.buildSingleView(data);
+  let nav = await utilities.getNav();
+  const vehicleName =
+    data.inv_year + " " + data.inv_make + " " + data.inv_model;
   res.render("./inventory/single", {
     title: vehicleName,
     nav,
     view,
-  })
-}
+  });
+};
+
+/* ***************************
+ *  Deliver add new classification view
+ * ************************** */
+invCont.buildAddClassification = async function (req, res, next) {
+  let nav = await utilities.getNav();
+  res.render("inventory/add-classification", {
+    title: "Add New Classification",
+    nav,
+    errors: null,
+  });
+};
+
+/* ****************************************
+ *  Process New Classification
+ * *************************************** */
+invCont.addNewClass = async function (req, res, next) {
+  let nav = await utilities.getNav();
+  let manageView = await utilities.buildManagementView();
+  const classificationSelect = await utilities.buildClassificationList();
+  const { classification_name } = req.body;
+  const newClassResult = await invModel.addNewClass(classification_name);
+
+  if (newClassResult) {
+    req.flash(
+      "notice",
+      `Successfully added classification "${classification_name}".`
+    );
+    res.status(201).render("inventory/management", {
+      title: "Vehicle Management",
+      nav,
+      manageView,
+      errors: null,
+      classificationSelect,
+    });
+  } else {
+    req.flash("notice", "Failed to add classification.");
+    res.status(501).render("inventory/add-classification", {
+      title: "Add New Classification",
+      nav,
+      errors: null,
+    });
+  }
+};
 
 module.exports = invCont;
